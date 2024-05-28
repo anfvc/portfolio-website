dc3811f6cba10d1968e3d913084b98e209f416af
andresvillay <andres.villay@dci-student.org>
2024-05-28 17:11:16 +0200

    formatted all code

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,16 +5,16 @@ function Footer({ mode }) {
   return (
     <footer className="w-full px-10 pt-40 md:px-20 overflow-x-hidden">
       <div
-        className={`w-full px-10 flex justify-center items-center border-t pt-20 pb-10 ${
+        className={`w-full flex justify-center items-center border-t pt-20 pb-10 ${
           mode ? "" : "border-black"
         } mx-auto max-w-screen-2xl`}
       >
         <div className="w-full flex items-center gap-2">
           <a
-            className={`text-4xl p-2 rounded-full ${
+            className={`text-4xl p-3 rounded-full border ${
               mode
-                ? "bg-black hover:bg-[#f0f0f0] hover:text-[#121212]"
-                : "bg-white hover:bg-[#121212] hover:text-[#f0f0f0]"
+                ? "bg-black border-white hover:bg-[#f0f0f0] hover:text-[#121212]"
+                : "bg-white border-black hover:bg-[#121212] hover:text-[#f0f0f0]"
             } transition-all ease-linear md:text-5xl`}
             href="https://github.com/anfvc"
             target="_blank"
@@ -22,10 +22,10 @@ function Footer({ mode }) {
             <FaGithub />
           </a>
           <a
-            className={`text-4xl p-2 rounded-full ${
+            className={`text-4xl p-3 rounded-full border ${
               mode
-                ? "bg-black hover:bg-[#f0f0f0] hover:text-[#121212]"
-                : "bg-white hover:bg-[#121212] hover:text-[#f0f0f0]"
+                ? "bg-black border-white hover:bg-[#f0f0f0] hover:text-[#121212]"
+                : "bg-white border-black hover:bg-[#121212] hover:text-[#f0f0f0]"
             } transition-all ease-linear md:text-5xl`}
             href="https://linkedin.com/in/avillay"
             target="_blank"
--- src/Components/Tech/Tech.jsx
+++ src/Components/Tech/Tech.jsx
@@ -2,7 +2,10 @@ import { technologies } from "./technologies.js";
 
 function Tech() {
   return (
-    <section id="stack" className="w-full flex justify-center py-24 scroll-m-20 ">
+    <section
+      id="stack"
+      className="w-full flex justify-center py-24 scroll-m-20 "
+    >
       <div className="flex flex-col px-10 py-10 gap-16 md:px-20">
         <div className="w-full flex justify-center">
           <h2 className="font-bold text-5xl">Tech Stack</h2>
@@ -11,8 +14,16 @@ function Tech() {
           <div className="w-full grid grid-cols-3 items-center gap-8 md:grid-cols-6 lg:grid-cols-12">
             {technologies.map((tech, i) => {
               return (
-                <div key={i} className="flex justify-center p-2 hover:scale-110 hover:transition hover:ease-in-out cursor-pointer">
-                  <img src={tech.image} alt={tech.name} loading="lazy" className="grayscale brightness-80 hover:grayscale-0 hover:duration-700 hover:ease-in-out" />
+                <div
+                  key={i}
+                  className="flex justify-center p-2 hover:scale-110 hover:transition hover:ease-in-out cursor-pointer"
+                >
+                  <img
+                    src={tech.image}
+                    alt={tech.name}
+                    loading="lazy"
+                    className="grayscale brightness-80 hover:grayscale-0 hover:duration-700 hover:ease-in-out"
+                  />
                 </div>
               );
             })}
