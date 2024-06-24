040755d50b79205bfeaa59bbef4bdbe934c562e2
andresvillay <andres.villay@dci-student.org>
2024-06-24 16:36:13 +0200

    added contact manager project

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -2,15 +2,25 @@ import euro from "../../assets/euroimg.png";
 import react from "../../assets/reactshopimg.png";
 import spotify from "../../assets/spotifycloneimg.png";
 import calculator from "../../assets/Calculatorimg.png";
+import manager from "../../assets/Managerimg.png";
 
 const projects = [
+  {
+    name: "Contact Manager",
+    description:
+      "Contact Manager is a full-stack application. Intended to show different functionalities such as: add, edit delete contacts from a user's perspective.",
+    link: "https://contact-manager-f-779t.onrender.com",
+    image: manager,
+    techUsed: ["Express", "MondoDB", "Node", "React", "TailwindCSS"],
+    github: "https://github.com/anfvc/address-book.git",
+  },
   {
     name: "The React Shop",
     description:
       "This is an online shop website I built called The React Shop. In this project I wanted to apply different concepts from React as well as some of its libraries such as react router dom. The goal was to recreate an online shop experience, from adding products to a wishlist to successfully making a purchase.",
     link: "https://spa-project-zd1l.onrender.com/",
     image: react,
-    techUsed: ["React", "React Router Dom", "JavaScript", "CSS", "Fetch API"],
+    techUsed: ["React", "React Router Dom", "CSS", "Fetch API"],
     github: "https://github.com/anfvc/spa-project.git",
   },
   {
